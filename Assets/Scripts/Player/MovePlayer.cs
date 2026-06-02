41e0b050d2ef60fd59b9c28926c13fb603fea432
MaxenceNk <mnkenyi@gaming.tech>
2026-06-02 17:41:30 +0200

    [ADD] sprint

--- Assets/Scripts/Player/MovePlayer.cs
+++ Assets/Scripts/Player/MovePlayer.cs
@@ -3,7 +3,18 @@ using UnityEngine.InputSystem;
 using static UnityEngine.UI.Image;
 public class MovePlayer : MonoBehaviour
 {
+    private float initialSpeed;
     [SerializeField] private float speed;
+    [SerializeField] private float sprintingMultiplier;
+    [SerializeField] private bool isSprinting = false;
+    private float sprintTime;
+    [SerializeField] private float sprintTimeLeft;
+    private bool isOutOfStamina = false;
+    private bool canSprint = false;
+
+    private bool movingX = false;
+    private bool movingZ = false;
+    
     private float moveInputX;
     private float moveInputZ;
    
@@ -40,6 +51,11 @@ public class MovePlayer : MonoBehaviour
         //cameraTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z);
         initialScale = transform.localScale;
         crouchScale = initialScale.y * 0.65f;
+        initialSpeed = 5f;
+        sprintingMultiplier = 1.4f;
+        speed = initialSpeed;
+        sprintTime = 5f;
+        sprintTimeLeft = sprintTime;
     }
     void Awake()
     {
@@ -58,16 +74,40 @@ public class MovePlayer : MonoBehaviour
         if (Keyboard.current != null)
         {
             if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+            {
                 moveInputX = 1f;
+                movingX = true;
+            }
             else if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+            {
                 moveInputX = -1f;
+                movingX = true;
+            }
+            else
+                movingX = false;
 
             if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+            {
                 moveInputZ = 1f;
+                movingZ = true;
+            }
             else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+            {
                 moveInputZ = -1f;
+                movingZ = true;
+            }
+            else
+                movingZ = false;
 
-            if(Keyboard.current.leftCtrlKey.isPressed)
+            if (movingX || movingZ)
+                canSprint = true;
+            else
+            {
+                canSprint = false;
+                StopSprinting();
+            }
+
+            if (Keyboard.current.leftCtrlKey.isPressed)
             {
                 Crouch();
             }
@@ -75,6 +115,40 @@ public class MovePlayer : MonoBehaviour
             if(Keyboard.current.leftCtrlKey.wasReleasedThisFrame)
             {
                 StandUp();
+            }
+
+            if(Keyboard.current.leftShiftKey.isPressed)
+            {
+                StartSprinting();
+            }
+
+            if (Keyboard.current.leftShiftKey.wasReleasedThisFrame)
+            {
+                StopSprinting();
+            }
+
+            if (isSprinting)
+            {
+                sprintTimeLeft -= Time.deltaTime;
+                if (sprintTimeLeft <= 0f)
+                {
+                    sprintTimeLeft = 0f;
+                    StopSprinting();
+                    isOutOfStamina = true;
+                }
+            }
+
+            if (isSprinting == false)
+            {
+                if (isOutOfStamina)
+                {
+                    if (sprintTimeLeft > 3f)
+                        isOutOfStamina = false;
+                }
+                if (sprintTimeLeft < 5f)
+                    sprintTimeLeft += Time.deltaTime / 2;
+                if (sprintTimeLeft > 5f)
+                    sprintTimeLeft = 5f;
             }
         }
     }
@@ -138,6 +212,28 @@ public class MovePlayer : MonoBehaviour
             isCrouched = false;
         }
         
+    }
+
+    void StartSprinting()
+    {
+        if (canSprint == false)
+            return;
+        if (isOutOfStamina)
+            return;
+        if ( isSprinting == false)
+        {
+            speed = initialSpeed * sprintingMultiplier;
+            isSprinting = true;
+        }
+    }
+
+    void StopSprinting()
+    {
+        if (isSprinting == true)
+        {
+            speed = initialSpeed;
+            isSprinting = false;
+        }
     }
 }
 
