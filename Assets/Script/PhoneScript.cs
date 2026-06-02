062706ee1a849dde1c5ca121421c735dc4abe825
Jauffrey ABISSET <jabisset@gaming.tech>
2026-06-02 15:25:36 +0200

    safe

--- Assets/Script/PhoneScript.cs
+++ Assets/Script/PhoneScript.cs
@@ -1,15 +1,32 @@
+using System;
+using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class PhoneScript : MonoBehaviour
 {
     public GameObject phone;
     public Light phoneLight;
+    public TextMeshProUGUI textBattery;
+
+    public float maxBattery = 100; // Max Battery (percentage)
+    public float currentBattery = 0; // Current Battery (percentage)
+    public float maxTime = 60; // Time in seconds
+    public float currentTimer = 0; // Current Timer  
+
+    private void Start()
+    {
+        currentBattery = maxBattery;
+        currentTimer = maxTime;
+        textBattery.text = ((int)currentBattery).ToString() + "%";
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             phone.SetActive(!phone.activeSelf);
+            textBattery.enabled = !textBattery.enabled;
 
             if (!phone.activeSelf)
             {
@@ -17,9 +34,28 @@ public class PhoneScript : MonoBehaviour
             }
         }
 
+        if (phoneLight.enabled == true)
+        {
+            currentTimer -= 8 * Time.deltaTime;
+        }
+        else if (phone.activeSelf)
+        {
+            currentTimer -= Time.deltaTime;
+        }
+
+        currentBattery = currentTimer * maxBattery / maxTime;
+        textBattery.text = ((int)currentBattery).ToString() + "%";
+
         if (Input.GetMouseButtonDown(0) && phone.activeSelf)
         {
             phoneLight.enabled = !phoneLight.enabled;
+        }
+
+        if(currentBattery <= 0)
+        {
+            currentBattery = 0;
+            phone.SetActive(false);
+            textBattery.enabled = false;
         }
     }
 }
