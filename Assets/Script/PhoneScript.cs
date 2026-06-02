e6c07e193535d7f70c9479d6e5e9e7b81806330e
Jauffrey ABISSET <jabisset@gaming.tech>
2026-06-02 11:11:34 +0200

    light ok

--- Assets/Script/PhoneScript.cs
+++ Assets/Script/PhoneScript.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PhoneScript : MonoBehaviour
 {
     public GameObject phone;
+    public Light phoneLight;
 
     void Update()
     {
-        if (Keyboard.current.aKey.wasPressedThisFrame)
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             phone.SetActive(!phone.activeSelf);
+
+            if (!phone.activeSelf)
+            {
+                phoneLight.enabled = false;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0) && phone.activeSelf)
+        {
+            phoneLight.enabled = !phoneLight.enabled;
         }
     }
 }
