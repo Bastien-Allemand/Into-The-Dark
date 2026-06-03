c915785027d114ebb11bbb3aa2b2101586713302
Ted974p <tpanechou@gaming.tech>
2026-06-03 12:33:50 +0200

    Add vision to monster

--- Assets/Scripts/Ghost/MonsterVisionScript.cs
+++ Assets/Scripts/Ghost/MonsterVisionScript.cs
@@ -1,55 +1,91 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class MonsterVisionScript : MonoBehaviour
 {
-    [SerializeField] public float rayonDetection = 5f;
+    [SerializeField] private LayerMask targetLayer;
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private Camera enemyCamera;
 
-    public float angleVision = 180f;
-    public float hauteurYeux = 1.0f;
-    [SerializeField] public LayerMask cibleLayer;
-    [SerializeField] public LayerMask obstacleLayer;
-
+    private GameObject targetObj;
     private bool PlayerFound = false;
+    [SerializeField] private float rayonDetection;
+    [SerializeField] private float chaseCD;
+    private float chaseActualCD;
 
-    //// Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Collider[] allColliders = Object.FindObjectsByType<Collider>(FindObjectsSortMode.None);
 
-        //RaycastHit hit;
-        //Debug.DrawRay(transform.position, transform.up, Color.red);
-        //if (Physics.Raycast(transform.position, transform.right, out hit, 10))
-        //{
-        //    Debug.Log("Raycast connect");
-        //}
-
-    }
-
-    void Update()
-    {
-
-        Vector3 origineVision = transform.position + Vector3.up * hauteurYeux;
-        Collider[] ciblesDansLeRayon = Physics.OverlapSphere(transform.position, rayonDetection, cibleLayer);
-
-        foreach (Collider cible in ciblesDansLeRayon)
+        foreach (Collider col in allColliders)
         {
-            Transform cibleTransform = cible.transform;
-
-            Vector3 directionVersCible = (cibleTransform.position - transform.position).normalized;
-
-            if (Vector3.Angle(transform.forward, directionVersCible) < angleVision / 2)
+            if ((targetLayer.value & (1 << col.gameObject.layer)) > 0)
             {
-                float distanceVersCible = Vector3.Distance(origineVision, cibleTransform.position);
-
-                if (!Physics.Raycast(origineVision, directionVersCible, distanceVersCible, obstacleLayer))
-                {
-                    Debug.DrawRay(origineVision, cibleTransform.position, Color.green);
-                }
-                else
-                {
-                    Debug.DrawRay(origineVision, cibleTransform.position, Color.red);
-                    Debug.Log($"Cible détectée : {cible.name} est vue depuis les yeux !");
-                }
+                targetObj = col.gameObject;
+                Debug.Log("targetObj " + targetObj.layer);
             }
         }
+
     }
+    void Update()
+    {
+        EnnemyChase();
+
+        if (!PlayerInFrustum())
+            return;
+
+        if (!RayConnectToPlayer())
+            return;
+
+        PlayerFound = true;
+        chaseActualCD = chaseCD;
+    }
+    void EnnemyChase()
+    {
+        if (!PlayerFound)
+            return;
+
+        chaseActualCD -= 2 * Time.deltaTime;
+
+        if (chaseActualCD < 0)
+            PlayerFound = false;
+    }
+    bool PlayerInFrustum()
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(enemyCamera);
+
+        Collider targetCollider = targetObj.GetComponent<Collider>();
+
+        return GeometryUtility.TestPlanesAABB(planes, targetCollider.bounds);
+    }
+    bool RayConnectToPlayer()
+    {
+        RaycastHit hit;
+        bool result = false;
+
+        if (targetObj == null) 
+            return result;
+        Vector3 direction = targetObj.transform.position - transform.position;
+
+        Vector3 directionNormalisee = direction.normalized;
+
+        Debug.DrawRay(transform.position, directionNormalisee, Color.blue);
+        if (Physics.Raycast(transform.position, directionNormalisee, out hit, 1000, -1))
+        {
+            if (!hit.collider.gameObject.layer.Equals(targetObj.layer))
+            {
+                result = false;
+                Debug.Log("obstacle touché " + hit.collider.gameObject.layer + " target layer :" );
+            }
+            else
+            {
+                result = true;
+                Debug.Log("player trouvé");
+            }
+        }
+        return result;
+    }
+
 }
