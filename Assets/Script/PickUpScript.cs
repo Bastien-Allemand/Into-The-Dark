f5afb7242b09029b4278d07af3594a606ae021e8
Ethan Faure <efaure@gaming.tech>
2026-06-04 14:38:30 +0200

    **[Update]** | pickable object work but can't replace cam

--- Assets/Script/PickUpScript.cs
+++ Assets/Script/PickUpScript.cs
@@ -2,22 +2,38 @@ using UnityEngine;
 
 public class PickUpScript : MonoBehaviour
 {
-    [SerializeField] private GameObject targetObject;
-    [SerializeField] private Transform handSocket;
+    private Transform handSocket;
 
     void Start()
     {
-        
+        GameObject socketObj = GameObject.Find("RightHandSocket");
+        if (socketObj != null)
+        {
+            handSocket = socketObj.transform;
+        }
+        else
+        {
+            Debug.LogError("RightHandSocket introuvable dans la scène !");
+        }
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            transform.SetParent(handSocket);
-            
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
+            if (handSocket != null)
+            {
+                transform.SetParent(handSocket);
+                
+                transform.localPosition = Vector3.zero;
+                transform.localRotation = Quaternion.identity;
+
+                Rigidbody rb = GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.isKinematic = true;
+                }
+            }
         }
     }
 }
