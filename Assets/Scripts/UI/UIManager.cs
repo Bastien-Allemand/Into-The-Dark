e15080b2f9a124a745af331f2c221784ef7d9711
M1jock <155654188+M1jock@users.noreply.github.com>
2026-06-08 12:52:58 +0200

    [ADD] UIManager [ADD] Init (not finish)

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,16 +1,83 @@
+using System;
+using TMPro;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject GO_controls;
+    [SerializeField] GameObject GO_Prefab_Keybind;
 
-    // Update is called once per frame
+    PlayerAction controls;
+
+    enum actionType
+    {
+        None,
+        Bool,
+        Axis1,
+        Vector2
+    }
+    private void Awake()
+    {
+        controls = InputManager.controls;
+        Init();
+    }
     void Update()
     {
-        
+
+    }
+    private actionType GetActionType(InputAction action)
+    {
+        actionType type = actionType.None;
+        switch (action.expectedControlType)
+        {
+            case "Button":
+                type = actionType.Bool;
+                break;
+            case "Axis":
+                type = actionType.Axis1;
+                break;
+            case "Vector2":
+                type = actionType.Vector2;
+                break;
+        }
+        return type;
+    }
+    private void Init()
+    {
+        GO_controls.SetActive(true);
+        Transform t = getContent(GO_controls.transform);
+        foreach (InputAction action in controls.GamePlay.Get())
+        {
+            if (action.name == "Look")
+                continue;
+
+
+
+            int boucle = 0;
+            switch(action.type)
+            {
+                case InputActionType.Value:
+                    GetActionType(action);
+                    break;
+                case InputActionType.Button:
+                    buffer.GetComponent<TextMeshPro>().text = action.name;
+                    break;
+                case InputActionType.PassThrough:
+                    //  nothing ...
+                    break;
+                default:
+                    break;
+            }
+            GameObject buffer = Instantiate(GO_Prefab_Keybind, t, false);
+            Debug.Log(action.name);
+        }
+    }
+    private Transform getContent(Transform of)
+    {
+        Transform result = of.Find("ViewPort");
+        result = result.Find("Content");
+        return result;
     }
 }
