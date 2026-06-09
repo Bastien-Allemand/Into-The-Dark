9901d8227b2c95d4d656526adee6d91c70f02ef6
Jauffrey ABISSET <jabisset@gaming.tech>
2026-06-09 13:01:43 +0200

    ADD   | Adding radar (in progress)
    Co-Authored-By: jonathan-LEFEVRE <184010157+jonathan-LEFEVRE@users.noreply.github.com>

--- Assets/Script/RadarScript.cs
+++ Assets/Script/RadarScript.cs
@@ -1,7 +1,20 @@
+using System.Threading;
 using UnityEngine;
 
 public class RadarScript : MonoBehaviour
 {
+    public GameObject radar;
+    public GameObject playerIcon;
+    public GameObject ghostIcon;
+
+    public Vector2 worldMin = new Vector2 (0, 0);
+    public Vector2 worldMax = new Vector2 (0, 0);
+
+    public RectTransform mapRadarRect;
+
+    public Transform player;
+    public Transform ghost;
+
     void Start()
     {
         
@@ -9,6 +22,28 @@ public class RadarScript : MonoBehaviour
 
     void Update()
     {
-        
+        if (radar.transform.parent != null)
+        {
+            radar.transform.localRotation = Quaternion.Euler(90f, 90f, 90f);
+        }
+
+        //playerIcon.anchoredPosition = WorldToRadar(player.position);
+        //ghostIcon.anchoredPosition = WorldToRadar(monster.position);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //radarUI.SetActive(!radarUI.activeSelf);
+        }
+    }
+
+    private Vector2 WorldToRadar(Vector3 worldPos)
+    {
+        float x = Mathf.InverseLerp(worldMin.x, worldMax.x, worldPos.x);
+        float y = Mathf.InverseLerp(worldMin.y, worldMax.y, worldPos.z);
+
+        float radarX = (x - 0.5f) * mapRadarRect.rect.width;
+        float radarY = (y - 0.5f) * mapRadarRect.rect.height;
+
+        return new Vector2(radarX, radarY);
     }
 }
