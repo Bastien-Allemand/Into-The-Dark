8ca16c3915bb4f61e16bd7d4d8fdfff15bd24706
Jauffrey ABISSET <jabisset@gaming.tech>
2026-06-09 17:51:49 +0200

    Add | Radar
    Co-Authored-By: jonathan-LEFEVRE <184010157+jonathan-LEFEVRE@users.noreply.github.com>

--- Assets/Script/RadarScript.cs
+++ Assets/Script/RadarScript.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadarScript : MonoBehaviour
 {
@@ -7,17 +8,23 @@ public class RadarScript : MonoBehaviour
     public GameObject playerIcon;
     public GameObject ghostIcon;
 
-    public Vector2 worldMin = new Vector2 (0, 0);
-    public Vector2 worldMax = new Vector2 (0, 0);
-
-    public RectTransform mapRadarRect;
+    public Vector2 worldMin = new Vector2 (-27f, -18.3f);
+    public Vector2 worldMax = new Vector2 (26.7f, 21.6f);
 
     public Transform player;
     public Transform ghost;
 
+    public Image radarImage;
+
+    private float radarWidth = 0.74f;
+    private float radarHeight = 0.71f;
+
     void Start()
     {
-        
+        RectTransform rt = radarImage.GetComponent<RectTransform>();
+
+        //radarWidth = rt.rect.width * 0.4f * 0.001f;
+        //radarHeight = rt.rect.height * 0.4f * 0.001f;
     }
 
     void Update()
@@ -27,12 +34,21 @@ public class RadarScript : MonoBehaviour
             radar.transform.localRotation = Quaternion.Euler(90f, 90f, 90f);
         }
 
-        //playerIcon.anchoredPosition = WorldToRadar(player.position);
-        //ghostIcon.anchoredPosition = WorldToRadar(monster.position);
+        Vector2 pos = WorldToRadar(player.position);
+        playerIcon.transform.localPosition =
+            new Vector3(pos.x, -0.5f, pos.y);
 
-        if (Input.GetKeyDown(KeyCode.R))
+        Vector2 pos2 = WorldToRadar(ghost.position);
+        ghostIcon.transform.localPosition =
+            new Vector3(pos2.x, -0.5f, pos2.y);
+
+        //Debug.Log(pos);
+        Debug.Log(radarWidth);
+        Debug.Log(radarHeight);
+
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            //radarUI.SetActive(!radarUI.activeSelf);
+            radarUI.SetActive(!radarUI.activeSelf);
         }
     }
 
@@ -41,8 +57,8 @@ public class RadarScript : MonoBehaviour
         float x = Mathf.InverseLerp(worldMin.x, worldMax.x, worldPos.x);
         float y = Mathf.InverseLerp(worldMin.y, worldMax.y, worldPos.z);
 
-        float radarX = (x - 0.5f) * mapRadarRect.rect.width;
-        float radarY = (y - 0.5f) * mapRadarRect.rect.height;
+        float radarX = (x - 0.5f) * radarWidth;
+        float radarY = (y - 0.5f) * radarHeight;
 
         return new Vector2(radarX, radarY);
     }
