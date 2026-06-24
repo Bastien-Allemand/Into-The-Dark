edbcc14f4252e9ea984e5670e67a8a4d78bebc1b
LilianBRR <lbarre@gaming.tech>
2026-06-24 16:43:18 +0200

    REF : Foot Steps finish (to adapt for ghost later)

--- Assets/Scripts/Ghost/FootSteps.cs
+++ Assets/Scripts/Ghost/FootSteps.cs
@@ -1,35 +1,57 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FootSteps : MonoBehaviour
 {
 
     [SerializeField] private GameObject footprintPrefab;
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float raycastDistance = 2f;
-    void Start()
+
+    [SerializeField] private float timeBetweenSteps = 2f;
+    [SerializeField] private float currentTimeSteps = 0f;
+
+    void Awake()
     {
-        
+        currentTimeSteps = 0f;
     }
     void Update()
     {
-        Vector3 rayStart = transform.position;
-        RaycastHit hit;
-        if (Physics.Raycast(rayStart, Vector3.down, out hit, raycastDistance, groundLayer))
+        CheckCanPlaceSteps();
+    }
+
+    void CheckCanPlaceSteps()
+    {
+        currentTimeSteps += Time.deltaTime;
+        if (currentTimeSteps > timeBetweenSteps)
         {
-
-            Debug.DrawLine(rayStart, hit.point, Color.red);
-
-            Vector3 spawnPosition = hit.point;
-            Quaternion spawnRotation = Quaternion.LookRotation(-hit.normal, transform.forward);
-
-            GameObject newDecal = Instantiate(footprintPrefab, spawnPosition, spawnRotation);
-
-            Destroy(newDecal, 10f);
+            currentTimeSteps = 0f;
+            Vector3 rayStart = playerTransform.position + Vector3.up * 0.5f;
+            RaycastHit hit;
+            if (Physics.Raycast(rayStart, Vector3.down, out hit, raycastDistance, groundLayer))
+            {
+                PlaceSteps(rayStart, hit);
+            }
+            else
+            {
+                Debug.DrawRay(rayStart, Vector3.down + Vector3.up * 0.5f, Color.green);
+            }
         }
-        else
-        {
-            Debug.DrawRay(rayStart, Vector3.back, Color.green);
-        }
+    }
+    void PlaceSteps(Vector3 rayStart, RaycastHit hit)
+    {
 
+        Debug.DrawLine(rayStart, hit.point, Color.red);
+
+        Vector3 spawnPosition = hit.point + (hit.normal * 0.005f);
+        Vector3 forwardDirection = Vector3.ProjectOnPlane(playerTransform.forward, hit.normal);
+
+        Quaternion spawnRotation = Quaternion.LookRotation(-forwardDirection, hit.normal);
+
+        GameObject newDecal = Instantiate(footprintPrefab, spawnPosition, spawnRotation);
+
+        Destroy(newDecal, 10f);
+       
     }
 }
