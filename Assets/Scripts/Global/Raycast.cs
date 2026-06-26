79c2b08c581b6aadb676bf139ea686ba2af55f8d
LilianBRR <lbarre@gaming.tech>
2026-06-26 10:55:40 +0200

    ref : raycast script boxcast to finish

--- Assets/Scripts/Global/Raycast.cs
+++ Assets/Scripts/Global/Raycast.cs
@@ -27,6 +27,18 @@ public class Raycast : MonoBehaviour
         return null;
     }
 
+    //static public RaycastResult CheckRaycast(Transform _transform,Vector3 _extents, Vector3 _dir,float _range)
+    //{
+    //    Vector3 origin = _transform.position;
+    //    //Ray ray = _cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+    //    //Debug.DrawRay(ray.origin, ray.direction * _range, Color.red, 2f);
+    //    if (Physics.BoxCast(origin, _extents,_dir,, _range))
+    //    {
+    //        return new RaycastResult(hit.collider.gameObject, hit.point, hit.normal, hit.distance);
+    //    }
+    //    return null;
+    //}
+
 
 }
 
