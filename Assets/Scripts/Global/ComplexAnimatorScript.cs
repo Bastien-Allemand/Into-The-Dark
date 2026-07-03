2a034feedda55fe47156dbf2dcda4e8e7087a4c0
Ethan Faure <efaure@gaming.tech>
2026-07-03 11:06:42 +0200

    REF : all animations end (need to implement to the new map

--- Assets/Scripts/Global/ComplexAnimatorScript.cs
+++ Assets/Scripts/Global/ComplexAnimatorScript.cs
@@ -8,48 +8,14 @@ public class ComplexAnimatorScript : MonoBehaviour
     private void Awake()
     {
         animator = GetComponent<Animator>();
-
-        if (animator == null)
-        {
-            Debug.LogError($"[{name}] Aucun Animator trouvé !");
-        }
-        else
-        {
-            Debug.Log($"[{name}] Animator trouvé : {animator.runtimeAnimatorController.name}");
-        }
     }
 
     public void Interact()
     {
-        Debug.Log($"[{name}] Interact appelé");
-
-        if (animator == null)
-        {
-            Debug.LogError($"[{name}] Animator NULL");
-            return;
-        }
-
         isOpen = !isOpen;
-
-        Debug.Log($"[{name}] IsOpen = {isOpen}");
 
         animator.SetBool("IsOpen", isOpen);
 
-        if (isOpen)
-        {
-            Debug.Log($"[{name}] Ouverture");
-
-            animator.speed = 1f;
-        }
-        else
-        {
-            Debug.Log($"[{name}] Fermeture");
-
-            animator.speed = -1f;
-
-            animator.Play("OpenAnim", 0, 1f);
-        }
-
-        Debug.Log($"[{name}] Etat actuel : {animator.GetCurrentAnimatorStateInfo(0).shortNameHash}");
+        Debug.Log($"IsOpen = {isOpen}");
     }
 }
