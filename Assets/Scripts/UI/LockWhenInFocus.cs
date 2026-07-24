047d530c00bab07bf9610cf156fa94d755cec452
Bastien Allemand--Leung Pin <ballemandleungpin@gaming.tech>
2026-07-24 15:20:42 +0200

    ADD
    working menus

--- Assets/Scripts/UI/LockWhenInFocus.cs
+++ Assets/Scripts/UI/LockWhenInFocus.cs
@@ -4,7 +4,17 @@ public class LockWhenInFocus : MonoBehaviour
 {
     void Start()
     {
+        lockMouse();
+    }
+
+    void lockMouse()
+    {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+    void unlockMouse()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
