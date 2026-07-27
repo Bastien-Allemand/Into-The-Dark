6c80c60ff89a80cbe3855741281fdc897b0391b1
M1jock <155654188+M1jock@users.noreply.github.com>
2026-07-27 12:51:17 +0200

    FIX SoundOutil.cs
    fix :
    - l'uniformité de la couleur pour chaque son.
    - fix le mat (créer un doublon au lieu d'utiliser toujours le même)
    - activer et desactiver le visuel avec le Manager

--- Assets/Scripts/Outils/Audio/SoundOutil.cs
+++ Assets/Scripts/Outils/Audio/SoundOutil.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 
 public class SoundOutil : MonoBehaviour
 {
-    private struct visualDebug
+    public class visualDebug
     {
         public GameObject _gameObject;
         public float rayon;
@@ -21,17 +21,26 @@ public class SoundOutil : MonoBehaviour
 
     private AudioSource audioSource;
 
-    private visualDebug visualMinSound;
-    private visualDebug visualMaxSound;
+    public visualDebug visualMinSound;
+    public visualDebug visualMaxSound;
+
+    private Color mColor;
 
     private bool lastState = false;
 
-    private void Start()
+    private void Awake()
     {
-
         audioSource = GetComponent<AudioSource>();
         if (audioSource)
         {
+            visualMinSound = new visualDebug();
+            visualMaxSound = new visualDebug();
+            mColor = new Color(
+                Random.value,
+                Random.value,
+                Random.value,
+                alpha
+            );
             CreateTransparentSphere(visualMaxSound, size.Max);
             CreateTransparentSphere(visualMinSound, size.Min);
             lastState = audioSource.isPlaying;
@@ -44,6 +53,11 @@ public class SoundOutil : MonoBehaviour
     }
     private void Update()
     {
+        if (!audioSource)
+        {
+            Destroy(this);
+            return;
+        }
         if (lastState != audioSource.isPlaying)
         {
             lastState = audioSource.isPlaying;
@@ -58,11 +72,29 @@ public class SoundOutil : MonoBehaviour
     }
     private void OnEnable()
     {
-
+        visualMinSound?._gameObject?.SetActive(true);
+        visualMaxSound?._gameObject?.SetActive(true);
     }
     private void OnDisable()
     {
+        visualMinSound?._gameObject?.SetActive(false);
+        visualMaxSound?._gameObject?.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        if (visualMinSound != null) DestroyVisual(visualMinSound._gameObject);
+        if (visualMaxSound != null) DestroyVisual(visualMaxSound._gameObject);
+    }
 
+    private void DestroyVisual(GameObject obj)  //  devrai être inutile car uniquement utiliser en Editor
+    {
+        if (obj == null)
+            return;
+#if UNITY_EDITOR
+        DestroyImmediate(obj);
+#else
+    Destroy(obj);
+#endif
     }
 
     private void sizeUpdate(visualDebug sphere, size size)
@@ -78,7 +110,6 @@ public class SoundOutil : MonoBehaviour
         {
             sphere.rayon = distance;
 
-            //   adapt le scale de base par rapport au parent pour obtenir une sphere
             Vector3 parentScale = transform.lossyScale;
             sphere._gameObject.transform.localScale = new Vector3(
                 1f / parentScale.x,
@@ -86,7 +117,6 @@ public class SoundOutil : MonoBehaviour
                 1f / parentScale.z
             );
 
-            //  augment la scale par rapport a la force du son
             float diameter = sphere.rayon * 2f;
             sphere._gameObject.transform.localScale *= diameter;
         }
@@ -102,40 +132,18 @@ public class SoundOutil : MonoBehaviour
         gameObject.rayon = 0;
         sizeUpdate(gameObject, size);
 
-
         Collider col = sphere.GetComponent<Collider>();
         if (col != null)
             Destroy(col);
 
-        {   // le mat utiliser (randomisation de la couleur et transparence, transparence pas encore terminer)
-
-            Material mat = Resources.Load<Material>("Materials/transparentTemplate");
+        {
+            Material mat = new Material(Resources.Load<Material>("Materials/transparentTemplate"));
             Debug.Log($"test {mat.name}");
 
-
-            Color randomColor = new Color(
-                Random.value,
-                Random.value,
-                Random.value,
-                alpha
-            );
-            //mat.color = randomColor;
-            mat.SetColor("_BaseColor", randomColor);
-
-            //mat.SetFloat("_Surface", 1);
-            //mat.SetFloat("_Blend", 0);
-            //mat.SetFloat("_ZWrite", 0);
-            //mat.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
-
-            //mat.SetFloat("_Cull", (float)UnityEngine.Rendering.CullMode.Off);
-
-
-            //mat.EnableKeyword("_SURFACE_TYPE_TRANSPARENT");
-            //mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.SetColor("_BaseColor", mColor);
             mat.renderQueue = (int)RenderQueue.Transparent;
 
             sphere.GetComponent<Renderer>().material = mat;
         }
-
     }
 }
